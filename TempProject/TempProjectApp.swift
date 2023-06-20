f7ca46ef0b12043379b152ce1588411dcbc67e94
이준후 <hprize04@gmail.com>
2023-06-20 11:29:16 +0900

    Initial Commit

---
+++ TempProject/TempProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempProjectApp.swift
+//  TempProject
+//
+//  Created by 이준후 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct TempProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
